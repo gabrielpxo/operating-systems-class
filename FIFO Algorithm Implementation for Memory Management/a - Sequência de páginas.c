4765c5b50a4f6410dbb5b64497c0202ed98671a5
Gabriel Paixão de Oliveira <gabrielpaixao1532@gmail.com>
2024-11-06 15:48:54 -0300

    Update a - Sequência de páginas.c

--- FIFO Algorithm Implementation for Memory Management/a - Sequência de páginas.c
+++ FIFO Algorithm Implementation for Memory Management/a - Sequência de páginas.c
@@ -1,5 +1,3 @@
-//Testar o algoritmo com diferentes sequências de testes e, de diferentes tamanhos e analisar os resultados
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -58,32 +56,32 @@ int main() {
 	int capacidade = 3; // Capacidade dos quadros
 	
 	//teste 1
-    int pages_um[] = {1, 3, 0, 3, 5, 6, 3}; // Sequência de páginas referenciadas
+    int pages_um[] = {1, 2, 3, 4, 5, 6, 7}; // Todos os elementos são diferentes
     int n = sizeof(pages_um) / sizeof(pages_um[0]);
     FIFO(pages_um, n, capacidade);
     
     //teste 2
-	int pages_dois[] = {2, 5, 1, 4, 3, 2, 6}; // Sequência de páginas referenciadas
+	int pages_dois[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}; // Há o dobro de elementos e são todos diferentes
 	int o = sizeof(pages_dois) / sizeof(pages_dois[0]);
 	FIFO(pages_dois, o, capacidade);
 	
 	//teste 3
-	int pages_tres[] = {1, 3, 5}; // Sequência de páginas referenciadas
+	int pages_tres[] = {1, 1, 2, 3, 4, 5, 6}; // 2 elementos são iguais
 	int p = sizeof(pages_tres) / sizeof(pages_tres[0]);
 	FIFO(pages_tres, p, capacidade);
 	
 	//teste 4
-	int pages_quatro[] = {1, 3, 0, 3, 5, 6, 3, 2, 1, 4, 8}; // Sequência de páginas referenciadas
+	int pages_quatro[] = {1, 1, 2, 3, 4, 1, 1}; // 4 elementos são iguais, mas são intercalados pela quantidade de quadros em elementos distintos
 	int q = sizeof(pages_quatro) / sizeof(pages_quatro[0]);
 	FIFO(pages_quatro, q, capacidade);
 	
 	//teste 5
-	int pages_cinco[] = {-1, 3, 0, -3, 5, -6, 3}; // Sequência de páginas referenciadas
+	int pages_cinco[] = {1, 1, 1, 1, 2, 3, 4}; // 4 elementos são iguais em sequência
 	int r = sizeof(pages_cinco) / sizeof(pages_cinco[0]);
 	FIFO(pages_cinco, r, capacidade);
 	
 	//teste 6
-	int pages_seis[] = {1000, 3000, 500, 2000, 1500, 6000, 3500}; // Sequência de páginas referenciadas
+	int pages_seis[] = {2, 3, 2, 4, 2, 5, 2}; // 4 elementos são iguais intercaladamente
 	int s = sizeof(pages_seis) / sizeof(pages_seis[0]);
 	FIFO(pages_seis, s, capacidade);
 
